c4594081ba8c5e5bd6a24d23c72026eb75822f76
Leandro Gavidia Santamaria <leandro.d3v@gmail.com>
2024-10-06 01:07:37 -0400

    feat: add gemini and gpt-4o-mini chats

--- app/api/gpt-4o-mini/route.ts
+++ app/api/gpt-4o-mini/route.ts
@@ -1,33 +1,20 @@
-import { OpenAIStream, StreamingTextResponse } from "ai";
-import { NextRequest } from "next/server";
-import { Configuration, OpenAIApi } from "openai-edge";
+import { openai } from '@ai-sdk/openai';
+import { streamText, convertToCoreMessages } from 'ai';
 
+export const maxDuration = 30;
 export const runtime = "edge";
 
 if (!process.env.OPENAI_API_KEY) {
     throw new Error("Missing environment variables, check the .env.example file")
 }
 
-const config = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY
-})
-
-const openai = new OpenAIApi(config)
-
-export async function POST(request: NextRequest) {
+export async function POST(request: Request) {
         const { messages } = await request.json();
 
-        const response = await openai.createChatCompletion({
-            model: "gpt-4o-mini",
-            stream: true,
-            messages,
-            max_tokens: 1024,
-            temperature: 0.7,
-            top_p: 1,
-            frequency_penalty: 0,
-            presence_penalty: 0,
-        })
+        const result = await streamText({
+            model: openai('gpt-4o-mini'),
+            messages: convertToCoreMessages(messages),
+          });
     
-        const stream = OpenAIStream(response);
-        return new StreamingTextResponse(stream)
+        return result.toDataStreamResponse();
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,26 @@
-import Image from "next/image";
+"use client"
+
+import { useState } from "react";
+import { Chat } from "./components/chat";
 
 export default function Home() {
+  const [model, setModel] = useState("gpt-4o-mini")
+
   return (
-    <div></div>
+    <div className="w-full h-full p-4 mx-auto grid grid-cols-1 row-auto border rounded-2xl border-zinc-900 px-4 gap-y-6">
+      <select 
+        onChange={e => setModel(e.target.value)} 
+        name="model" 
+        id="model" 
+        defaultValue="gpt-4o-mini"
+        className="bg-transparent border border-zinc-900 rounded-md p-2 cursor-pointer"
+      >
+        <option value="gemini" className="text-black">
+          Gemini
+        </option>
+        <option value="gpt-4o-mini" selected className="text-black">gpt-4o-mini</option>
+      </select>
+      <Chat model={model} />
+    </div>
   );
 }
