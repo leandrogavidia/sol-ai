5217c6d327dbe4f82167e0a50e290177773cbef2
Leandro Gavidia Santamaria <leandro.d3v@gmail.com>
2024-10-09 14:51:27 -0400

    chore: update chat gpt api

--- app/api/sol-ai/route.ts
+++ app/api/sol-ai/route.ts
@@ -2,14 +2,13 @@ import { openai } from '@ai-sdk/openai';
 import { streamText, convertToCoreMessages } from 'ai';
 
 export const maxDuration = 30;
-export const runtime = "edge";
 
 if (!process.env.OPENAI_API_KEY) {
     throw new Error("Missing environment variables, check the .env.example file")
 }
 
-export async function POST(request: Request) {
-    const { messages } = await request.json();
+export async function POST(req: Request) {
+    const { messages } = await req.json();
 
     let systemContent = "";
 
