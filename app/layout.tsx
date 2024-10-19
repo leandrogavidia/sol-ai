3e08fe9eb2d1cb5246e6239e1477e909ae99a071
Leandro Gavidia Santamaria <leandro.d3v@gmail.com>
2024-10-19 11:06:06 -0400

    config: add pwa config

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,9 +17,20 @@ const geistMono = localFont({
   weight: "100 900",
 });
 
+
 export const metadata: Metadata = {
   title: "Sol AI",
   description: "AI-powered solana virtual assistant",
+  generator: "Next.js",
+  manifest: "/manifest.json",
+  keywords: ["nextjs", "next14", "pwa", "next-pwa"],
+  themeColor: [{ media: "(prefers-color-scheme: dark)", color: "#fff" }],
+  viewport:
+    "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
+  icons: [
+    { rel: "apple-touch-icon", url: "/sol-ai-logo.png" },
+    { rel: "icon", url: "/sol-ai-logo.png" },
+  ],
 };
 
 export default function RootLayout({
