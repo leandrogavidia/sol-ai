a71b64d6ebd56e3907bfa16982ad2d972bb3e777
Leandro Gavidia Santamaria <leandro.d3v@gmail.com>
2024-11-09 01:14:06 -0400

    feature: add feedback status and loading

--- app/components/forms/feedback-form.tsx
+++ app/components/forms/feedback-form.tsx
@@ -3,8 +3,16 @@
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
+import { cn } from "@/app/lib/utils";
+import { Spinner } from "../spinner";
 
 type FeedbackType = "New feature" | "UI/UX" | "New project" | "Error" | "Other";
+enum FeedbackStatus {
+  DEFAULT = "DEFAULT",
+  SUCCESS = "SUCCESS",
+  ERROR = "ERROR",
+}
 
 interface Feedback {
   title: string;
@@ -13,6 +21,10 @@ interface Feedback {
 }
 
 export function FeedbackForm() {
+  const [status, setStatus] = useState<FeedbackStatus>(FeedbackStatus.DEFAULT);
+  const [isLoading, setIsLoading] = useState(false);
+  const [message, setMessage] = useState("");
+
   const schema = z.object({
     title: z.string().min(1, "Title is required"),
     feedback: z.string().min(1, "Feedback is required"),
@@ -23,12 +35,43 @@ export function FeedbackForm() {
     register,
     handleSubmit,
     formState: { errors },
+    reset
   } = useForm<Feedback>({
     resolver: zodResolver(schema),
   });
 
-  const onSubmit = (data: Feedback) => {
-    console.log(data);
+  const onSubmit = async ({ title, feedback, type }: Feedback) => {
+    setIsLoading(true);
+    try {
+      const res = await fetch("/api/feedback", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title,
+          feedback,
+          type,
+          email: "solaitest@gmail.com",
+        }),
+      });
+      const data = await res.json();
+
+      if (res.ok) {
+        setStatus(FeedbackStatus.SUCCESS);
+        setMessage(data.message);
+        reset()
+      } else {
+        setStatus(FeedbackStatus.ERROR);
+        setMessage(data.message);
+      }
+    } catch (e) {
+      console.error("Error:", e);
+      setStatus(FeedbackStatus.ERROR);
+      setMessage((e as Error).message);
+    } finally {
+      setIsLoading(false);
+    }
   };
   return (
     <form
@@ -90,9 +133,35 @@ export function FeedbackForm() {
           {errors && errors.type ? `* ${errors.type.message}` : ""}
         </span>
       </div>
-      <button className="w-full min-h-10 rounded-md border-solana-purple border-2 text-white font-semibold bg-solana-purple transition-all hover:opacity-80">
-        Send
+      <button
+        className={cn(
+          "w-full min-h-10 rounded-md border-solana-purple  font-semibold  transition-all hover:opacity-80 px-2 py-2 text-white",
+          isLoading
+            ? "bg-gray-800  cursor-default pointer-events-none select-none"
+            : "bg-solana-purple  border-2"
+        )}
+      >
+        {isLoading ? (
+          <div className="flex justify-center items-center gap-x-3">
+            <Spinner />
+            <p>Sending...</p>
+          </div>
+        ) : (
+          "Send"
+        )}
       </button>
+
+      {message && (
+        <span
+          className={cn(
+            "mt-4 mx-auto text-sm font-semibold",
+            status === FeedbackStatus.SUCCESS && "text-solana-green",
+            status === FeedbackStatus.ERROR && "text-red-600"
+          )}
+        >
+          {message}
+        </span>
+      )}
     </form>
   );
 }
