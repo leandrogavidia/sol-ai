7df703c69db85d7190ede1b5c889c29f2cf5ed45
leandrogavidia <leandro.d3v@gmail.com>
2025-03-25 20:59:09 -0400

    feat: add Diatype font files and update layout to use new font; add thumbnail image and remove old roadmap PDF

---
+++ app/components/button.tsx
@@ -0,0 +1,31 @@
+import { MouseEventHandler, ReactNode } from "react";
+import { cn } from "../lib/utils";
+
+interface Button {
+  children: ReactNode;
+  type: "button" | "reset" | "submit";
+  className?: string;
+  onClick?: MouseEventHandler<HTMLButtonElement>;
+}
+
+export function Button({
+  children,
+  type,
+  className = "",
+  onClick,
+  ...props
+}: Button) {
+  return (
+    <button
+      className={cn(
+        "min-w-24 max-w-max min-h-7 py-2 rounded-full text-lg font-normal border border-white max",
+        className
+      )}
+      type={type}
+      onClick={onClick}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+}
