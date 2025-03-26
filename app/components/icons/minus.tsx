6b9d14e8e240fe276a532b401266c3f2e7f3513b
leandrogavidia <leandro.d3v@gmail.com>
2025-03-25 22:35:06 -0400

    feat: add new icons and context options; implement collapsible question items in the main page

---
+++ app/components/icons/minus.tsx
@@ -0,0 +1,9 @@
+import { cn } from "@/app/lib/utils";
+
+export function Minus({ className }: { className?: string }) {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className={cn("size-6", className)}>
+            <path fillRule="evenodd" d="M4.25 12a.75.75 0 0 1 .75-.75h14a.75.75 0 0 1 0 1.5H5a.75.75 0 0 1-.75-.75Z" clipRule="evenodd" />
+        </svg>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,45 +1,49 @@
-import { auth } from "@/auth";
-import { redirect } from "next/navigation";
-import Image from "next/image";
-import Link from "next/link";
+
+import { ContextOption } from "./components/context-option";
+import { Plus } from "./components/icons/plus";
+import { QuestionItem } from "./components/question-item";
 
 export default async function Home() {
-  const session = await auth();
-    
-  if (session) {
-    redirect("/early-access");
-  }
+  const contextOptionList = [
+    "Projects & communities",
+    "Hackathones",
+    "Blinks"
+  ]
 
+  const questions = [
+    {
+      title: "",
+      content: ""
+    }
+  ]
   return (
-    <div className="w-full h-full p-4 mx-auto flex flex-col text-center justify-center items-center px-4">
-      <Image
-        src="/logo.png"
-        width={854}
-        height={210}
-        title="Logo"
-        alt="Logo"
-        className="cursor-pointer max-w-36 h-auto w-full"
-        priority
-      />
-      <div className="mt-6">
-        <h1 className="text-2xl font-semibold">The AI of Solana</h1>
-        <h2 className="text-lg">AI-powered Solana virtual assistant</h2>
-      </div>
-      <div className="flex justify-center items-center gap-x-5 mt-8">
-        <Link
-          href="https://drive.google.com/file/d/1NIviRcoH2NS4yvge6yWiQzOCaLRLl0YL/view"
-          target="_blank"
-        >
-          <button className="border border-white text-white flex justify-center items-center min-h-10 px-4 py-2 rounded-md transition-all font-semibold hover:bg-white hover:text-black ">
-            Watch video demo
-          </button>
-        </Link>
-        <Link href="https://forms.gle/zpT1tK7KKXeiSzCh8" target="_blank">
-          <button className="border border-white text-black bg-white flex justify-center items-center min-h-10 px-4 py-2 rounded-md transition-all font-semibold hover:bg-transparent hover:text-white ">
-            Get early access
-          </button>
-        </Link>
-      </div>
-    </div>
+    <>
+      <section className="mt-16 flex flex-col justify-center items-start text-left">
+        <h1 className="text-4xl font-bold">The AI of Solana</h1>
+        <h2 className="text-xl font-normal mt-4 mb-7">AI-powered Solana virtual assistant</h2>
+        <p className="text-base font-light">Sol AI is the bridge that connects people with the Solana ecosystem: projects, communities, hackathons, blinks, and much more. Our goal is to facilitate learning about the ecosystem and help onboard more users. To achieve this, we use an LLM assistant that allows anyone to obtain up-to-date information about Solana projects and innovations.</p>
+      </section>
+
+      <section className="my-20 flex flex-col justify-center items-start text-left">
+        <h2 className="text-4xl font-bold">Specialized context in Solana</h2>
+        <h3 className="text-xl font-normal mt-4">We keep adding</h3>
+        <div className="w-full flex flex-col justify-center items-start gap-y-5 mt-12">
+          {
+            contextOptionList.map((item) => <ContextOption text={item} key={item} />)
+          }
+        </div>
+      </section>
+
+      <section className="flex flex-col justify-center items-start text-left">
+        <h2 className="text-4xl font-bold">Sol AI FAQs</h2>
+        <h3 className="text-xl font-normal mt-4">Frequently asked questions</h3>
+        <div className="w-full flex flex-col justify-center items-start gap-y-5 my-12">
+            <QuestionItem title="Question 01" content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat nesciunt velit illum deleniti, minus necessitatibus facilis soluta sint quibusdam facere at inventore suscipit quam quos architecto. Laboriosam nesciunt aut tenetur!" />
+            
+        </div>
+
+
+      </section>
+    </>
   );
 }
