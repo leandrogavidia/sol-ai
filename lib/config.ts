1278c28e4fe668fa2b4c05a228cdb260915a74ba
Leandro Gavidia Santamaria <93232139+leandrogavidia@users.noreply.github.com>
2025-05-27 01:54:10 -0400

    Update config.ts

--- lib/config.ts
+++ lib/config.ts
@@ -1,6 +1,6 @@
 export const config = {
     socialMedia: {
-        discord: "https://discord.gg/RJAXrMAG",
+        discord: "https://discord.gg/W79E4GqXyX",
         x: "https://x.com/_sol_ai",
         github: "https://github.com/Sol-AI-Lab",
         instagram: "https://instagram.com/_sol_ai"
