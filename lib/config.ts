45a4aeb1b7f1834769ea84e5e003be530635759f
leandrogavidia <leandro.d3v@gmail.com>
2025-07-08 14:19:25 -0400

    chore: update instagram's link

--- lib/config.ts
+++ lib/config.ts
@@ -9,7 +9,7 @@ export const config = {
         discord: "https://discord.gg/W79E4GqXyX",
         x: "https://x.com/solailabs_",
         github: "https://github.com/Sol-AI-Lab",
-        instagram: "https://instagram.com/_sol_ai"
+        instagram: "https://instagram.com/solailabs_"
     },
     rpcUrl
 }
